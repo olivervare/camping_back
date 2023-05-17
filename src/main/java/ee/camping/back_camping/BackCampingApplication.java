6aeee8f08fbd92ad140fde0755b1fc128dee9239
Madis Bobrovski <115641439+madisbobo@users.noreply.github.com>
2023-05-17 12:31:23 +0300

    Initial commit

---
+++ src/main/java/ee/camping/back_camping/BackCampingApplication.java
@@ -0,0 +1,13 @@
+package ee.camping.back_camping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCampingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackCampingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/camping/back_camping/BackCampingApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.camping.back_camping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCampingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
